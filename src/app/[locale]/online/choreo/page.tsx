685ad71f6e3b5814c5fdd6b0cbbc6fb70874375a
Dmitriy Strok <all1son4>
2026-05-06 13:28:41 +0300

    Close sales

--- src/app/[locale]/online/choreo/page.tsx
+++ src/app/[locale]/online/choreo/page.tsx
@@ -2,34 +2,21 @@ import type { Metadata } from "next";
 import { useLocale, useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
 
-import ChoreoCard from "@/components/cards/ChoreoCard";
-import TextContentCard from "@/components/cards/TextContentCard";
-import Button from "@/components/common/Button";
 import StructuredData from "@/components/common/StructuredData";
 import SvgAsset from "@/components/common/SvgAsset";
-import Contacts from "@/components/other/Contacts";
 import { SELLABLE_PRODUCTS_LIST } from "@/constants/sellable-products";
 import { buildPageMetadata, normalizedSiteUrl, seoTargetLocale } from "@/lib/seo";
 
 import { buildCheckoutOffersStructuredData } from "../_shared/structured-data";
 import { getChoreos, getOnlineSuggestions } from "./constants";
 import {
-  AboutChoreoCards,
-  AboutChoreoSection,
-  AboutChoreoTitle,
-  ButtonBox,
-  ChoreoSection,
   Date,
   DateBox,
-  Description,
-  DescriptionParagraph,
-  From,
   IconBox,
   ImageBox,
   InfoBoxGroup,
   IntroductionSection,
   MobileImagesBox,
-  SpecialWrapper,
   TextBox,
   Title,
 } from "./page.styles";
@@ -102,26 +89,11 @@ export default function ChoreoPage() {
         <TextBox>
           <Title>{t("hero.title")}</Title>
 
-          <Description>
-            <DescriptionParagraph>{t("hero.description.1")}</DescriptionParagraph>
-            <DescriptionParagraph>{t("hero.description.2")}</DescriptionParagraph>
-          </Description>
-
           <InfoBoxGroup>
             <DateBox>
-              <From>{t("hero.startLabel")}</From>
-              <Date>{t("hero.startValue")}</Date>
-            </DateBox>
-
-            <DateBox>
-              <From>{t("hero.salesLabel")}</From>
-              <Date>{t("hero.salesValue")}</Date>
+              <Date>{t("hero.closedValue")} &#128546;</Date>
             </DateBox>
           </InfoBoxGroup>
-
-          <ButtonBox>
-            <Button buttonText={t("hero.button")} href="#choreo-section" />
-          </ButtonBox>
         </TextBox>
 
         <MobileImagesBox>
@@ -166,22 +138,93 @@ export default function ChoreoPage() {
           />
         </IconBox>
       </IntroductionSection>
-      <SpecialWrapper>
-        <AboutChoreoSection>
-          <AboutChoreoTitle>{t("about.title")}</AboutChoreoTitle>
-          <AboutChoreoCards>
-            {onlineSuggestions.map(({ id, ...suggestion }) => (
-              <TextContentCard key={id} {...suggestion} />
-            ))}
-          </AboutChoreoCards>
-        </AboutChoreoSection>
-        <ChoreoSection id="choreo-section">
-          {choreos.map(({ id, ...choreo }) => (
-            <ChoreoCard key={id} {...choreo} />
-          ))}
-        </ChoreoSection>
-        <Contacts bgColor="rgba(200, 204, 210, 0.4)" />
-      </SpecialWrapper>
     </>
+    // <>
+    //   <StructuredData data={choreographyProductsStructuredData} />
+    //   <IntroductionSection>
+    //     <TextBox>
+    //       <Title>{t("hero.title")}</Title>
+
+    //       <Description>
+    //         <DescriptionParagraph>{t("hero.description.1")}</DescriptionParagraph>
+    //         <DescriptionParagraph>{t("hero.description.2")}</DescriptionParagraph>
+    //       </Description>
+
+    //       <InfoBoxGroup>
+    //         <DateBox>
+    //           <From>{t("hero.startLabel")}</From>
+    //           <Date>{t("hero.startValue")}</Date>
+    //         </DateBox>
+
+    //         <DateBox>
+    //           <From>{t("hero.salesLabel")}</From>
+    //           <Date>{t("hero.salesValue")}</Date>
+    //         </DateBox>
+    //       </InfoBoxGroup>
+
+    //       <ButtonBox>
+    //         <Button buttonText={t("hero.button")} href="#choreo-section" />
+    //       </ButtonBox>
+    //     </TextBox>
+
+    //     <MobileImagesBox>
+    //       <ImageBox id="mobile-only-image-box">
+    //         <SvgAsset
+    //           src="/svg/OnlineChoreoPageBackgroundPhoto.webp"
+    //           width={794}
+    //           height={989}
+    //           sizes="(max-width: 767px) 100vw, 0px"
+    //           priority
+    //           unoptimized
+    //         />
+    //       </ImageBox>
+
+    //       <IconBox id="mobile-only-icon-box">
+    //         <SvgAsset
+    //           src="/svg/TelegramChoreo.webp"
+    //           width={401}
+    //           height={421}
+    //           sizes="(max-width: 767px) 50vw, 0px"
+    //         />
+    //       </IconBox>
+    //     </MobileImagesBox>
+
+    //     <ImageBox id="desktop-only-image-box">
+    //       <SvgAsset
+    //         src="/svg/OnlineChoreoPageBackgroundPhoto.webp"
+    //         width={794}
+    //         height={989}
+    //         sizes="(max-width: 767px) 0px, (max-width: 880px) 490px, (max-width: 1140px) 540px, (max-width: 1240px) 640px, 794px"
+    //         priority
+    //         unoptimized
+    //       />
+    //     </ImageBox>
+
+    //     <IconBox id="desktop-only-icon-box">
+    //       <SvgAsset
+    //         src="/svg/TelegramChoreo.webp"
+    //         width={401}
+    //         height={421}
+    //         sizes="(max-width: 767px) 0px, (max-width: 880px) 240px, (max-width: 1140px) 260px, (max-width: 1240px) 320px, 401px"
+    //       />
+    //     </IconBox>
+    //   </IntroductionSection>
+    //   <SpecialWrapper>
+    //     <AboutChoreoSection>
+    //       <AboutChoreoTitle>{t("about.title")}</AboutChoreoTitle>
+    //       <AboutChoreoCards>
+    //         {onlineSuggestions.map(({ id, ...suggestion }) => (
+    //           <TextContentCard key={id} {...suggestion} />
+    //         ))}
+    //       </AboutChoreoCards>
+    //     </AboutChoreoSection>
+    //     <ChoreoSection id="choreo-section">
+    //       {choreos.map(({ id, ...choreo }) => (
+    //         <ChoreoCard key={id} {...choreo} />
+    //       ))}
+    //     </ChoreoSection>
+    //     <Contacts bgColor="rgba(200, 204, 210, 0.4)" />
+    //   </SpecialWrapper>
+    // </>
   );
 }
--- src/app/[locale]/online/constants.tsx
+++ src/app/[locale]/online/constants.tsx
@@ -31,28 +31,28 @@ export const getOnlineCoursesArray = (t: Translate): InteractiveCourseCard[] => 
     buttonText: t("cards.firstTouch.button"),
     buttonHref: "/online/first-touch",
   },
-  {
-    id: "choreo",
-    title: t("cards.choreo.title"),
-    topRowContent: (
-      <ContentStack $gap="16px">
-        <DetailText>{t("cards.choreo.description.1")}</DetailText>
-        <DetailText>{t("cards.choreo.description.2")}</DetailText>
-      </ContentStack>
-    ),
-    bottomRowContent: (
-      <ContentStack $gap="20px">
-        <ContentStack>
-          <DetailText>{t("cards.choreo.startLabel")}</DetailText>
-          <DetailValueText>{t("cards.choreo.startValue")}</DetailValueText>
-        </ContentStack>
-        <ContentStack>
-          <DetailText>{t("cards.choreo.salesLabel")}</DetailText>
-          <DetailValueText>{t("cards.choreo.salesValue")}</DetailValueText>
-        </ContentStack>
-      </ContentStack>
-    ),
-    buttonText: t("cards.choreo.button"),
-    buttonHref: "/online/choreo",
-  },
+  // {
+  //   id: "choreo",
+  //   title: t("cards.choreo.title"),
+  //   topRowContent: (
+  //     <ContentStack $gap="16px">
+  //       <DetailText>{t("cards.choreo.description.1")}</DetailText>
+  //       <DetailText>{t("cards.choreo.description.2")}</DetailText>
+  //     </ContentStack>
+  //   ),
+  //   bottomRowContent: (
+  //     <ContentStack $gap="20px">
+  //       <ContentStack>
+  //         <DetailText>{t("cards.choreo.startLabel")}</DetailText>
+  //         <DetailValueText>{t("cards.choreo.startValue")}</DetailValueText>
+  //       </ContentStack>
+  //       <ContentStack>
+  //         <DetailText>{t("cards.choreo.salesLabel")}</DetailText>
+  //         <DetailValueText>{t("cards.choreo.salesValue")}</DetailValueText>
+  //       </ContentStack>
+  //     </ContentStack>
+  //   ),
+  //   buttonText: t("cards.choreo.button"),
+  //   buttonHref: "/online/choreo",
+  // },
 ];
